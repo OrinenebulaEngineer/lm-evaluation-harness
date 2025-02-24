e6b0e4a4518950fc2246056e9b4dd1038899c75c
OrinenebulaEngineer <orinnebula@gmail.com>
2025-02-24 16:12:23 +0330

    update

--- lm_eval/tasks/khayyam/utils.py
+++ lm_eval/tasks/khayyam/utils.py
@@ -5,22 +5,22 @@ def process_data(dataframe : pd.DataFrame) :
     def _process_data(doc):
         dataset = Dataset.from_pandas(doc)
 
-        if doc['key'] == 1.0:
-            doc['key'] = 'Choice 1'
-        elif doc['key'] == 2.0:
-            doc['key'] = 'Choice 2'
-        elif doc['key'] == 3.0:
-            doc['key'] = 'Choice 3'
-        elif doc['key'] == 4.0:
-            doc['key'] = 'Choice 4'
+        if dataset['key'] == 1.0:
+            dataset['key'] = 'Choice 1'
+        elif dataset['key'] == 2.0:
+            dataset['key'] = 'Choice 2'
+        elif dataset['key'] == 3.0:
+            dataset['key'] = 'Choice 3'
+        elif dataset['key'] == 4.0:
+            dataset['key'] = 'Choice 4'
 
         out_doc = {
-            "Question" : doc['Question Body'],
-            'Choice 1' : doc['Choice 1'],
-            'Choice 2' : doc['Choice 2'],
-            'Choice 3' : doc['Choice 3'],
-            'Choice 4' : doc['Choice 4'],
-            'Answer'   : doc['key']
+            "Question" : dataset['Question Body'],
+            'Choice 1' : dataset['Choice 1'],
+            'Choice 2' : dataset['Choice 2'],
+            'Choice 3' : dataset['Choice 3'],
+            'Choice 4' : dataset['Choice 4'],
+            'Answer'   : dataset['key']
         }
 
         return out_doc

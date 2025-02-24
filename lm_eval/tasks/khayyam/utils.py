f94b4fc65273dfdb0485445d68d711a5d556541e
OrinenebulaEngineer <orinnebula@gmail.com>
2025-02-24 16:45:21 +0330

    update

--- lm_eval/tasks/khayyam/utils.py
+++ lm_eval/tasks/khayyam/utils.py
@@ -1,24 +1,25 @@
 import pandas as pd
 from datasets import Dataset
 
-def process_data(dataframe):
+def process_data(dataset):
     # dataset = Dataset.from_pandas(dataframe)
 
     def _process_data(doc):
         # Ensure doc is a DataFrame before further processing
         # if not isinstance(doc, pd.DataFrame):
         #     doc = pd.DataFrame(doc)
+        doc['key'] = int(doc['Key'])
 
 
         
-        if doc['Key'] == 1.0:
-            doc['Key'] = 'Choice 1'
-        elif doc['Key'] == 2.0:
-            doc['Key'] = 'Choice 2'
-        elif doc['Key'] == 3.0:
-            doc['Key'] = 'Choice 3'
-        elif doc['Key'] == 4.0:
-            doc['Key'] = 'Choice 4'
+        # if doc['Key'] == 1.0:
+        #     doc['Key'] = 'Choice 1'
+        # elif doc['Key'] == 2.0:
+        #     doc['Key'] = 'Choice 2'
+        # elif doc['Key'] == 3.0:
+        #     doc['Key'] = 'Choice 3'
+        # elif doc['Key'] == 4.0:
+        #     doc['Key'] = 'Choice 4'
 
         # Create the output dictionary for each document
         out_doc = {
@@ -33,7 +34,7 @@ def process_data(dataframe):
         return out_doc
     
     # Apply the function to the dataframe
-    return dataframe.map(_process_data)
+    return dataset.map(_process_data)
 
 # Assuming `dataframe` is your pandas DataFrame
 # processed_data = process_data(dataframe)

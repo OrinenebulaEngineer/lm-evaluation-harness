ceff82ec730ae2e0af13a9726dd37489effbbea1
OrinenebulaEngineer <orinnebula@gmail.com>
2025-02-24 14:17:32 +0330

    added utils.py fore retrun current path

---
+++ lm_eval/tasks/khayyam/utils.py
@@ -0,0 +1,12 @@
+import os
+
+#return local path for local task
+def get_data_path(file_name):
+
+    # Get the directory of the current script
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Build the relative path from the script's location
+    data_path = os.path.join(current_dir, file_name)
+
+    return data_path

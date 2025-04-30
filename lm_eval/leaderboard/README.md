5b6202b8aadefa78c9ee1bd1572334182bff161e
OrinenebulaEngineer <orinnebula@gmail.com>
2025-04-30 14:01:08 +0330

    update

---
+++ lm_eval/leaderboard/README.md
@@ -0,0 +1,15 @@
+---
+
+title: Your Space Title
+emoji: 🚀
+colorFrom: indigo
+colorTo: blue
+sdk: gradio
+sdk_version: 4.24.0
+app_file: app.py
+space_type: gradio_app
+---
+
+
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ lm_eval/leaderboard/manage_request.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class EvalRequest:
+    """This class represents one evaluation request file."""
+
+    model_arg: str
+    json_filepath: str
+    weight_type: str = "Original"
+    model_type: Optional[str] = None  # pretrained, fine-tuned, etc. - define your own categories in
+    precision: str = ""  # float16, bfloat16
+    revision: str = "main"  # commit hash
+    submitted_time: Optional[str] = (
+        "2024"  # random date just so that we can still order requests by date
+    )
+    likes: Optional[int] = 0
+    params: Optional[int] = None
+    license: Optional[str] = ""
+    base_model: Optional[str] = ""
+    private: Optional[bool] = False
+    status: str= None
+
+    def get_model_args(self):
+        """Edit this function if you want to manage more complex quantization issues. You'll need to map it to
+        the evaluation suite you chose.
+        """
+        model_args = f"pretrained={self.model},revision={self.revision}"
+
+        if self.precision in ["float16", "bfloat16"]:
+            model_args += f",dtype={self.precision}"
+
+        # Quantized models need some added config, the install of bits and bytes, etc
+        else:
+            raise Exception(f"Unknown precision {self.precision}.")
+
+        return model_args

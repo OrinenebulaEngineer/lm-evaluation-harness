4287dac5fe8f8075d6ed6096e6acfaa25d13d940
OrinenebulaEngineer <orinnebula@gmail.com>
2025-05-06 15:58:49 +0330

    add sm func

--- leaderboard/flask_eval_after_submit.py
+++ leaderboard/flask_eval_after_submit.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from evaluate_by_harness import evaluate
 
 app = Flask(__name__)
 
@@ -8,7 +9,9 @@ def evaluate():
     model = data.get('model')
     print(f"🚀 Evaluation triggered for model: {model}")
 
-    return jsonify({"status": "evaluation started", "model": data.get("model")})
+    # Call the evaluate_tasks function from the evaluate module
+    eval_response = evaluate(model )
+    return jsonify({"status": "evaluation started", "model": model, "eval_result" : eval_response})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

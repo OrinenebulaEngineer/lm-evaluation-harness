6d2e4bcbcf3a27c95a327c16ecb71f5f838f84f9
OrinenebulaEngineer <orinnebula@gmail.com>
2025-05-07 10:31:35 +0330

    update

--- leaderboard/utils_leaderboard.py
+++ leaderboard/utils_leaderboard.py
@@ -115,7 +115,7 @@ def write_to_jsonl(results, jsonl_path:str):
 
     # Define source and destination paths relative to current script
     source_path = current_dir  / "result.jsonl"
-    destination_path = current_dir / "HF_leaderboard" / "result.jsonl"
+    destination_path = current_dir / "HF_leaderboard" / "results.jsonl"
     # Make sure destination directory exists
     destination_path.parent.mkdir(parents=True, exist_ok=True)
 
